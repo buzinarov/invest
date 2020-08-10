0f734fef465e93df63b85be11039620cc156a2ef
unknown <buzinarov@gmail.com>
2020-08-10 13:34:38 -0300

    bora

--- invest.py
+++ invest.py
@@ -12,12 +12,12 @@ import os
 from selenium.webdriver.chrome.options import Options
 
 
-chrome_options = Options()
+chrome_options = webdriver.ChromeOptions()
 
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no--sandbox")
+chrome_options.add_argument("--no-sandbox")
 
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
